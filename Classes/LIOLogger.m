fcd66a38fc4a585d750820851085b2723a2b85a9
Superbil <superbil@gmail.com>
2016-03-12 00:54:20 +0800

    Fix coding style

--- Classes/LIOLogger.m
+++ Classes/LIOLogger.m
@@ -23,20 +23,17 @@
 #import "LIOLogger.h"
 
 @interface LIOLogger ()
-
 @property (nonatomic, assign) BOOL wasInitialized;
-@property (nonatomic, assign) NSUInteger calendarUnitFlags;
+@property (nonatomic, assign) NSCalendarUnit calendarUnitFlags;
 @property (nonatomic, strong) NSDictionary *streamList;
 @property (nonatomic, strong) GCDAsyncSocket *tcpSocket;
-
-
 @end
 
 @implementation LIOLogger
 
 #pragma mark - init methods
 
-+ (instancetype) sharedInstance {
++ (instancetype)sharedInstance {
     static dispatch_once_t onceToken = 0;
     static id _sharedInstance = nil;
 
@@ -47,33 +44,33 @@
     return _sharedInstance;
 }
 
-- (NSString *) loggerName {
+- (NSString *)loggerName {
     return @"cocoa.lumberjack.liologger";
 }
 
-- (BOOL) initialize {
+- (BOOL)initialize {
 
     if (!self.wasInitialized) {
-        self.calendarUnitFlags = (NSCalendarUnitYear   |
-                NSCalendarUnitMonth  |
-                NSCalendarUnitDay    |
-                NSCalendarUnitHour   |
-                NSCalendarUnitMinute |
-                NSCalendarUnitSecond);
+        self.calendarUnitFlags = (NSCalendarUnitYear
+                                  | NSCalendarUnitMonth
+                                  | NSCalendarUnitDay
+                                  | NSCalendarUnitHour
+                                  | NSCalendarUnitMinute
+                                  | NSCalendarUnitSecond);
 
-        self.streamList = @{
-                @(DDLogFlagError)   : @"error",
-                @(DDLogFlagWarning) : @"warn",
-                @(DDLogFlagInfo)    : @"info",
-                @(DDLogFlagDebug)   : @"debug",
-                @(DDLogFlagVerbose) : @"verbose",
-        };
+        self.streamList = @{@(DDLogFlagError)   : @"error",
+                            @(DDLogFlagWarning) : @"warn",
+                            @(DDLogFlagInfo)    : @"info",
+                            @(DDLogFlagDebug)   : @"debug",
+                            @(DDLogFlagVerbose) : @"verbose",
+                            };
 
         NSLog(@"[log.io] connecting to %@:%ld", self.host, (long) self.port);
 
-        self.tcpSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+        self.tcpSocket = [[GCDAsyncSocket alloc] initWithDelegate:self
+                                                    delegateQueue:dispatch_get_main_queue()];
         NSError *error = nil;
-        if ([self.tcpSocket connectToHost:self.host onPort:(uint16_t) self.port error:&error]) {
+        if ([self.tcpSocket connectToHost:self.host onPort:(uint16_t)self.port error:&error]) {
 
             for (NSString *key in self.streamList) {
                 [self sendRequest:[NSString stringWithFormat:@"+node|%@|%@\r\n", self.nodeName, self.streamList[key]]];
@@ -90,26 +87,29 @@
 
 #pragma mark - logging methods
 
-- (void) sendRequest:(NSString *)msg {
-    [self.tcpSocket writeData:[msg dataUsingEncoding:NSUTF8StringEncoding] withTimeout:-1 tag:1];
+- (void)sendRequest:(NSString *)msg {
+    NSData *msgData = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    [self.tcpSocket writeData:msgData withTimeout:-1 tag:1];
 }
 
-- (NSString *) getMessageDateTimeString:(DDLogMessage *)logMessage {
-    NSDateComponents *components = [[NSCalendar autoupdatingCurrentCalendar] components:(NSCalendarUnit) self.calendarUnitFlags fromDate:logMessage->_timestamp];
+- (NSString *)getMessageDateTimeString:(DDLogMessage *)logMessage {
+    NSDateComponents *components =
+    [[NSCalendar autoupdatingCurrentCalendar] components:self.calendarUnitFlags
+                                                fromDate:logMessage->_timestamp];
     NSTimeInterval epoch = [logMessage->_timestamp timeIntervalSinceReferenceDate];
-    int milliseconds = (int) ((epoch - floor(epoch)) * 1000);
+    int milliseconds = (int)((epoch - floor(epoch)) * 1000);
 
     return [NSString stringWithFormat:@"%04ld-%02ld-%02ld %02ld:%02ld:%02ld:%03d",
-                                      (long) components.year,
-                                      (long) components.month,
-                                      (long) components.day,
-                                      (long) components.hour,
-                                      (long) components.minute,
-                                      (long) components.second,
+                                      (long)components.year,
+                                      (long)components.month,
+                                      (long)components.day,
+                                      (long)components.hour,
+                                      (long)components.minute,
+                                      (long)components.second,
                                       milliseconds];
 }
 
-- (void) logMessage:(DDLogMessage *)logMessage {
+- (void)logMessage:(DDLogMessage *)logMessage {
 
     if (![self initialize]) {
         NSLog(@"[log.io] not initialized!");
@@ -118,25 +118,26 @@
 
     if (logMessage && self.streamList[@(logMessage->_flag)] != nil) {
         NSString *message = _logFormatter ? [_logFormatter formatLogMessage:logMessage] : logMessage->_message;
-        [self sendRequest:[NSString stringWithFormat:@"+log|%@|%@|%ld|%@ %@\r\n", self.streamList[@(logMessage->_flag)],
-                                                     self.nodeName,
-                                                     (long) logMessage->_flag,
-                                                     [self getMessageDateTimeString:logMessage],
-                                                     message]];
+        [self sendRequest:[NSString stringWithFormat:@"+log|%@|%@|%ld|%@ %@\r\n",
+                           self.streamList[@(logMessage->_flag)],
+                           self.nodeName,
+                           (long)logMessage->_flag,
+                           [self getMessageDateTimeString:logMessage],
+                           message]];
     }
 }
 
 #pragma mark - gcd methods
 
-- (void) socket:(GCDAsyncSocket *)sender didConnectToHost:(NSString *)host port:(UInt16)port {
+- (void)socket:(GCDAsyncSocket *)sender didConnectToHost:(NSString *)host port:(UInt16)port {
     NSLog(@"[log.io] socket connected on host!");
 }
 
-- (void) socketDidSecure:(GCDAsyncSocket *)sock {
+- (void)socketDidSecure:(GCDAsyncSocket *)sock {
     NSLog(@"[log.io] socket secured on host!");
 }
 
-- (void) socketDidDisconnect:(GCDAsyncSocket *)sock withError:(NSError *)err {
+- (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(NSError *)err {
     NSLog(@"[log.io] socket disconnected from host: (%@)", err);
 }
 
